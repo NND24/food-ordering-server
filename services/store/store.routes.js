d560cd9a6d6a809e350a92858cacfcf226e2dd8c
Dat Nguyen <=>
2025-03-22 22:26:26 +0700

    feat: Update get all store rating

--- services/store/store.routes.js
+++ services/store/store.routes.js
@@ -22,6 +22,7 @@ const {
   addToppingToGroup,
   removeToppingFromGroup,
   deleteToppingGroup,
+  getAllStoreRating,
   // createDish,
   // createStore,
   // createToppingGroup,
@@ -45,9 +46,10 @@ router.get("/:store_id", getStoreInformation); // CHECK
 // Dish routes
 router.get("/:store_id/dish", getAllDish); // CHECK // MOD
 router.get("/dish/:dish_id", getDish); // CHECK
-router.get("/dish/:dish_id/rating/avg", getAvgRating) // CHECK
+router.get("/dish/:dish_id/rating/avg", getAvgRating); // CHECK
 router.get("/dish/:dish_id/rating/", getAllRating); // CHECK
-router.get("/:store_id/rating/avg", getAvgStoreRating) // CHECK
+router.get("/:store_id/rating/avg", getAvgStoreRating); // CHECK
+router.get("/:storeId/rating", getAllStoreRating);
 router.get("/dish/:dish_id/topping", getToppingFromDish); // CHECK
 router.post("/dish/:dish_id/topping", addToppingToDish); // CHECK
 // router.post("/:store_id/dish/add", createDish);
@@ -55,7 +57,7 @@ router.post("/dish/:dish_id/topping", addToppingToDish); // CHECK
 
 // Topping routes
 router.get("/:store_id/topping", getAllTopping); // CHECK
-router.get("/topping-group/:group_id", getTopping);  // CHECK
+router.get("/topping-group/:group_id", getTopping); // CHECK
 router.post("/topping-group/:group_id/topping", addToppingToGroup); // CHECK
 router.delete("/topping-group/:group_id/topping/:topping_id", removeToppingFromGroup); // CHECK
 router.delete("/topping-group/:group_id", deleteToppingGroup); // CHECK
@@ -64,7 +66,7 @@ router.post("/:store_id/topping/create", createToppingGroup); // CHECK
 // router.put("/:store_id/topping/update", updateToppingGroup);
 
 // Category routes
-router.get("/:store_id/category", getAllCategory);  // CHECK
+router.get("/:store_id/category", getAllCategory); // CHECK
 router.get("/category/:category_id", getCategory); // CHECK
 // router.post("/:store_id/category/add", createCategory);
 // router.put("/:store_id/category/update", updateCategory);
@@ -77,6 +79,6 @@ router.get("/staff/:staff_id", getStaff); // CHECK
 
 // Order routes
 router.get("/:store_id/order", getAllOrder); // CHECK
-router.get("/order/:order_id", getOrder);  // CHECK
+router.get("/order/:order_id", getOrder); // CHECK
 
 module.exports = router;
